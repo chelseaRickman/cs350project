335c81a00e1ecb30bb9258f3604822889ec7747b
Chelsea Rickman <chelsearaerickman@gmail.com>
2020-11-22 08:19:23 -0800

    lays out restructure example

--- cs350team12/src/cs350f20project/controller/cli/parser/Create.java
+++ cs350team12/src/cs350f20project/controller/cli/parser/Create.java
@@ -3,6 +3,7 @@ package cs350f20project.controller.cli.parser;
 import cs350f20project.controller.command.A_Command;
 
 /*
+ * This class handles all the CREATE commands
 22 CREATE POWER CATENARY id1 WITH POLES idn+
 23 CREATE POWER POLE id1 ON TRACK id2 DISTANCE number FROM ( START | END )
 24 CREATE POWER STATION id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA coordinates_delta WITH ( SUBSTATION | SUBSTATIONS )idn+
@@ -39,21 +40,175 @@ public class Create extends ParserBase{
 		if(token == null)
 			return tokens.InvalidToken();
 		if(token.equalsIgnoreCase("POWER")) {
-			Power power = new Power(tokens);
-			return power.parse();
+			return createPower();
 		}
-		if(token.equalsIgnoreCase("STOCK")) {
-			/*
-			 * Make STOCK class
-			 * Create create a new instance of STOCK and send our tokens there.
-			 */
+		else if(token.equalsIgnoreCase("STOCK")) {
+			return createStock();
 		}
-		if(token.equalsIgnoreCase("TRACK")) {
-			/*
-			 * Make TRACK class
-			 * Create create a new instance of TRACK and send our tokens there.
-			 */
+		else if(token.equalsIgnoreCase("TRACK")) {
+			return createTrack();
 		}
+		
 		return checkArgs(token);
+	}
+	
+	// Determines which POWER method needs to be called
+	public A_Command createPower() {
+		String nextToken = tokens.getNext();
+		if(nextToken == null)
+			return tokens.InvalidToken();
+		
+		if(nextToken.equalsIgnoreCase("CATENARY"))
+			return powerCatenary();
+		if(nextToken.equalsIgnoreCase("POLE"))
+			return powerPole();
+		if(nextToken.equalsIgnoreCase("STATION"))
+			return powerStation();
+		if(nextToken.equalsIgnoreCase("SUBSTATION"))
+			return powerSubstation();
+		
+		return tokens.InvalidToken();	
+	}
+	
+	// Determines which STOCK method needs to be called
+	public A_Command createStock() {
+		String nextToken = tokens.getNext();
+		if(nextToken == null)
+			return tokens.InvalidToken();
+	
+		if(nextToken.equalsIgnoreCase("CAR"))
+			return stockCar();
+		if(nextToken.equalsIgnoreCase("ENGINE"))
+			return stockEngine();
+		
+		return tokens.InvalidToken();
+	}
+	
+	// Determines which TRACK method needs to be called
+	public A_Command createTrack() {
+		String nextToken = tokens.getNext();
+		if(nextToken == null)
+			return tokens.InvalidToken();
+		if(nextToken.equalsIgnoreCase("BRIDGE"))
+			return trackBridge();
+		if(nextToken.equalsIgnoreCase("CROSSING"))
+			return trackCrossing();
+		if(nextToken.equalsIgnoreCase("CROSSOVER"))
+			return trackCrossover();
+		if(nextToken.equalsIgnoreCase("CURVE"))
+			return trackCurve();
+		if(nextToken.equalsIgnoreCase("END"))
+			return trackEnd();
+		if(nextToken.equalsIgnoreCase("LAYOUT"))
+			return trackLayout();
+		if(nextToken.equalsIgnoreCase("ROUNDHOUSE"))
+			return trackRoundhouse();
+		if(nextToken.equalsIgnoreCase("STRAIGHT"))
+			return trackStraight();
+		if(nextToken.equalsIgnoreCase("SWITCH"))
+			return trackSwitch();
+		
+		return tokens.InvalidToken();
+	}
+	
+	// CREATE POWER commands
+	
+	private A_Command powerCatenary() {
+		// 22 CREATE POWER CATENARY id1 WITH POLES idn+
+		return null;
+	}
+	
+	private A_Command powerPole() {
+		// 23 CREATE POWER POLE id1 ON TRACK id2 DISTANCE number FROM ( START | END )
+		return null;
+	}
+	
+	private A_Command powerStation() {
+		// 24 CREATE POWER STATION id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA coordinates_delta WITH ( SUBSTATION | SUBSTATIONS )idn+
+		return null;
+	}
+	
+	private A_Command powerSubstation() {
+		// 25 CREATE POWER SUBSTATION id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA coordinates_delta WITH CATENARIES idn+
+		return null;
+	}
+	
+	// CREATE STOCK commands
+	
+	private A_Command stockCar() {
+		/*
+		Can create private helper methods from here to handle the different cars. 
+		Maybe can use Tokenizer.get(index) to look at the type of car. So at this point tokens.get(1) should return "BOX" for 28
+		28 CREATE STOCK CAR id AS BOX CommandCreateStockCarBox
+		29 CREATE STOCK CAR id AS CABOOSE CommandCreateStockCarCaboose
+		30 CREATE STOCK CAR id AS FLATBED CommandCreateStockCarFlatbed
+		31 CREATE STOCK CAR id AS PASSENGER CommandCreateStockCarPassenger
+		32 CREATE STOCK CAR id AS TANK CommandCreateStockCarTank
+		33 CREATE STOCK CAR id AS TENDER CommandCreateStockCarTender
+		*/
+		return null;
+	}
+	
+	private A_Command stockEngine() {
+		// 34 CREATE STOCK ENGINE id1 AS DIESEL ON TRACK id2 DISTANCE number FROM ( START | END ) FACING ( START | END ) CommandCreateStockEngineDiesel
+		return null;
+	}
+	
+	// CREATE TRACK commands
+	
+	private A_Command trackBridge() {
+		/*
+		 * Can create helper methods for TRACK BRIDGE and BRIDGE DRAW
+		 * Can use Tokenizer.get(index) to check whether it is BRIDGE or BRIDGE DRAW
+		 * 39 CREATE TRACK BRIDGE DRAW id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA START coordinates_delta1 END coordinates_delta2 ANGLE angle
+		 * 40 CREATE TRACK BRIDGE id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA START coordinates_delta1 END coordinates_delta2
+		 */
+		
+		return null;
+	}
+	
+	private A_Command trackCrossing() {
+		// 41 CREATE TRACK CROSSING id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA START coordinates_delta1 END coordinates_delta2
+		return null;
+	}
+	
+	private A_Command trackCrossover() {
+		// 42 CREATE TRACK CROSSOVER id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA START coordinates_delta1 END coordinates_delta2 START coordinates_delta3 END coordinates_delta4
+		return null;
+	}
+	
+	private A_Command trackCurve() {
+		// 43 CREATE TRACK CURVE id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA START coordinates_delta1 END coordinates_delta2 ( ( DISTANCE ORIGIN number ) | ( ORIGIN coordinates_delta3 ) )
+		return null;
+	}
+	
+	private A_Command trackEnd() {
+		// 44 CREATE TRACK END id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA START coordinates_delta1 END coordinates_delta2
+		return null;
+	}
+	
+	private A_Command trackLayout() {
+		// 45 CREATE TRACK LAYOUT id1 WITH TRACKS idn+
+		return null;
+	}
+	
+	private A_Command trackRoundhouse() {
+		// 46 CREATE TRACK ROUNDHOUSE id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA ORIGIN coordinates_delta1 ANGLE ENTRY angle1 START angle2 END angle3 WITH integer SPURS LENGTH number1 TURNTABLE LENGTH number2
+		return null;
+	}
+	
+	private A_Command trackStraight() {
+		// 47 CREATE TRACK STRAIGHT id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA START coordinates_delta1 END coordinates_delta2
+		return null;
+	}
+	
+	private A_Command trackSwitch() {
+		/*
+		 * Can create helper methods for TRACK SWITCH TURNOUT and TRACK SWITCH WYE
+		 * Can use Tokenizer.get(index) to check whether it is TURNOUT  or WYE
+		 * 48 CREATE TRACK SWITCH TURNOUT id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) STRAIGHT DELTA START coordinates_delta1 END coordinates_delta2 CURVE DELTA START coordinates_delta3 END coordinates_delta4 DISTANCE ORIGIN number
+		 * 49 CREATE TRACK SWITCH WYE id1 REFERENCE ( coordinates_world | ( '$' id2 ) ) DELTA START coordinates_delta1 END coordinates_delta2 DISTANCE ORIGIN number1 DELTA START coordinates_delta3 END coordinates_delta4 DISTANCE ORIGIN number2
+		 */
+		return null;
 	}
 }
--- cs350team12/src/cs350f20project/controller/cli/parser/Do.java
+++ cs350team12/src/cs350f20project/controller/cli/parser/Do.java
@@ -4,6 +4,7 @@ import cs350f20project.controller.command.A_Command;
 import cs350f20project.controller.command.behavioral.CommandBehavioralBrake;
 
 /*
+This class handles all the DO commands
 2  DO BRAKE id
 6  DO SELECT DRAWBRIDGE id POSITION ( UP | DOWN )
 7  DO SELECT ROUNDHOUSE id POSITION angle ( CLOCKWISE | COUNTERCLOCKWISE )
@@ -25,12 +26,11 @@ public class Do extends ParserBase{
 		if(token.equalsIgnoreCase("BRAKE"))
 			return brake(tokens);
 		if(token.equalsIgnoreCase("SELECT")) {
-			Select select = new Select(tokens);
-			return select.parse();
+			return doSelect();
 		}
 		if(token.equalsIgnoreCase("SET")) {
-			Set s = new Set(tokens);
-			return s.parse();
+			return doSet();
+			
 		}
 		return checkArgs(token);
 	}
@@ -43,9 +43,72 @@ public class Do extends ParserBase{
 		
 	}
 	
+	// Determines which SELECT method needs to be called
+	public A_Command doSelect() {
+		String nextToken = tokens.getNext();
+		if(nextToken == null)
+			return tokens.InvalidToken();
+		else if(nextToken.equalsIgnoreCase("DRAWBRIDGE"))
+			return selectDrawbridge();
+		else if(nextToken.equalsIgnoreCase("ROUNDHOUSE"))
+			return selectRoundhouse();
+		else if(nextToken.equalsIgnoreCase("SWITCH"))
+			return selectSwitch();
+		
+		return tokens.InvalidToken();
+	}
+	
+	// Determines which SET method needs to be called
+	public A_Command doSet() {
+		String nextToken = tokens.getNext();
+		if(nextToken == null)
+			return tokens.InvalidToken();
+		if(nextToken.equalsIgnoreCase("REFERENCE"))
+			return setReference();
+		else {
+			return setId();
+		}
+	}
+	
+	// DO SELECT commands
+	
+	private A_Command selectDrawbridge() {
+		// 6  DO SELECT DRAWBRIDGE id POSITION ( UP | DOWN )
+		return null;
+	}
+	
+	private A_Command selectRoundhouse() {
+		// 7  DO SELECT ROUNDHOUSE id POSITION angle ( CLOCKWISE | COUNTERCLOCKWISE )
+		return null;
+	}
+	
+	private A_Command selectSwitch() {
+		// 8  DO SELECT SWITCH id PATH ( PRIMARY | SECONDARY )
+		return null;
+	}
+	
+	// DO SET commands
+	
+	private A_Command setReference() {
+		// 12 DO SET REFERENCE ENGINE id
+		return null;
+	}
+	
+	private A_Command setId() {
+		/* 11 DO SET id DIRECTION ( FORWARD | BACKWARD )
+		 * 15 DO SET id SPEED number
+		 * Can create private helper methods for each of these rules, can use Tokenizer.get(index) to see if "DIRECTION" or "SPEED"
+		 * if neither "DIRECTION" or "SPEED", then invalid token
+		 */
+		
+		return null;
+	}
+	
 	//Check for standard Java variable name, underscore included
 	public boolean isStringStandardJavVar(String str) 
 	{ 
 	    return (str.matches("[a-zA-Z_$]")); 
 	}
+	
+	
 }
