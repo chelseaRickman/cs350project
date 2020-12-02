34f9096041fdfe63adfda83c676569f626972bb2
Micah Johnson <micahwj>
2020-12-01 16:38:56 -0800

    Added Checks::checkKeyword
    new function checks if the token is a keyword

--- cs350team12/src/cs350f20project/controller/cli/parser/Checks.java
+++ cs350team12/src/cs350f20project/controller/cli/parser/Checks.java
@@ -156,4 +156,312 @@ public class Checks {
 	{ 
 		return SourceVersion.isName(str);
 	}
+	
+	public static boolean checkKeyword(String str) {
+		boolean key = false;
+		if(str.equalsIgnoreCase("BOX"))
+			key = true;
+		if(str.equalsIgnoreCase("ACTUATOR"))
+			key = true;
+		if(str.equalsIgnoreCase("ACTUATORS"))
+			key = true;
+		if(str.equalsIgnoreCase("AND"))
+			key = true;
+		if(str.equalsIgnoreCase("ANGLE"))
+			key = true;
+		if(str.equalsIgnoreCase("AS"))
+			key = true;
+		if(str.equalsIgnoreCase("AT"))
+			key = true;
+		if(str.equalsIgnoreCase("BACKSLASH"))
+			key = true;
+		if(str.equalsIgnoreCase("BACKWARD"))
+			key = true;
+		if(str.equalsIgnoreCase("BRAKE"))
+			key = true;
+		if(str.equalsIgnoreCase("BRIDGE"))
+			key = true;
+		if(str.equalsIgnoreCase("CABOOSE"))
+			key = true;
+		if(str.equalsIgnoreCase("CAR"))
+			key = true;
+		if(str.equalsIgnoreCase("CATENARIES"))
+			key = true;
+		if(str.equalsIgnoreCase("CATENARY"))
+			key = true;
+		if(str.equalsIgnoreCase("CAUTION"))
+			key = true;
+		if(str.equalsIgnoreCase("CLOCKWISE"))
+			key = true;
+		if(str.equalsIgnoreCase("CLOSE"))
+			key = true;
+		if(str.equalsIgnoreCase("COLLISIONS"))
+			key = true;
+		if(str.equalsIgnoreCase("COLON"))
+			key = true;
+		if(str.equalsIgnoreCase("COMMIT"))
+			key = true;
+		if(str.equalsIgnoreCase("COUNTERCLOCKWISE"))
+			key = true;
+		if(str.equalsIgnoreCase("COUPLE"))
+			key = true;
+		if(str.equalsIgnoreCase("CREATE"))
+			key = true;
+		if(str.equalsIgnoreCase("CROSSBUCK"))
+			key = true;
+		if(str.equalsIgnoreCase("CROSSING"))
+			key = true;
+		if(str.equalsIgnoreCase("CROSSOVER"))
+			key = true;
+		if(str.equalsIgnoreCase("CURVE"))
+			key = true;
+		if(str.equalsIgnoreCase("DEFAULT"))
+			key = true;
+		if(str.equalsIgnoreCase("DELTA"))
+			key = true;
+		if(str.equalsIgnoreCase("DIESEL"))
+			key = true;
+		if(str.equalsIgnoreCase("DIGIT"))
+			key = true;
+		if(str.equalsIgnoreCase("DISABLE"))
+			key = true;
+		if(str.equalsIgnoreCase("DISTANCE"))
+			key = true;
+		if(str.equalsIgnoreCase("DO"))
+			key = true;
+		if(str.equalsIgnoreCase("DOLLAR"))
+			key = true;
+		if(str.equalsIgnoreCase("DOT"))
+			key = true;
+		if(str.equalsIgnoreCase("DOWN"))
+			key = true;
+		if(str.equalsIgnoreCase("DRAW"))
+			key = true;
+		if(str.equalsIgnoreCase("DRAWBRIDGE"))
+			key = true;
+		if(str.equalsIgnoreCase("ELECTRIC"))
+			key = true;
+		if(str.equalsIgnoreCase("ENABLE"))
+			key = true;
+		if(str.equalsIgnoreCase("END"))
+			key = true;
+		if(str.equalsIgnoreCase("ENGINE"))
+			key = true;
+		if(str.equalsIgnoreCase("ENTRY"))
+			key = true;
+		if(str.equalsIgnoreCase("EOF"))
+			key = true;
+		if(str.equalsIgnoreCase("EQUAL"))
+			key = true;
+		if(str.equalsIgnoreCase("FACING"))
+			key = true;
+		if(str.equalsIgnoreCase("FLATBED"))
+			key = true;
+		if(str.equalsIgnoreCase("FLOW"))
+			key = true;
+		if(str.equalsIgnoreCase("FOR"))
+			key = true;
+		if(str.equalsIgnoreCase("FORCE"))
+			key = true;
+		if(str.equalsIgnoreCase("FORWARD"))
+			key = true;
+		if(str.equalsIgnoreCase("FROM"))
+			key = true;
+		if(str.equalsIgnoreCase("GATE"))
+			key = true;
+		if(str.equalsIgnoreCase("GREATER"))
+			key = true;
+		if(str.equalsIgnoreCase("HASH"))
+			key = true;
+		if(str.equalsIgnoreCase("HEIGHT"))
+			key = true;
+		if(str.equalsIgnoreCase("IDENTIFIER"))
+			key = true;
+		if(str.equalsIgnoreCase("INTEGER"))
+			key = true;
+		if(str.equalsIgnoreCase("LAYOUT"))
+			key = true;
+		if(str.equalsIgnoreCase("LENGTH"))
+			key = true;
+		if(str.equalsIgnoreCase("LESS"))
+			key = true;
+		if(str.equalsIgnoreCase("LETTER"))
+			key = true;
+		if(str.equalsIgnoreCase("LIGHT"))
+			key = true;
+		if(str.equalsIgnoreCase("LOCATE"))
+			key = true;
+		if(str.equalsIgnoreCase("MAP"))
+			key = true;
+		if(str.equalsIgnoreCase("META_CLOCK"))
+			key = true;
+		if(str.equalsIgnoreCase("META_DO"))
+			key = true;
+		if(str.equalsIgnoreCase("META_EXIT"))
+			key = true;
+		if(str.equalsIgnoreCase("META_RUN"))
+			key = true;
+		if(str.equalsIgnoreCase("META_SCHEDULE"))
+			key = true;
+		if(str.equalsIgnoreCase("META_WAIT"))
+			key = true;
+		if(str.equalsIgnoreCase("NETWORK"))
+			key = true;
+		if(str.equalsIgnoreCase("NORTH"))
+			key = true;
+		if(str.equalsIgnoreCase("OCCUPANCY"))
+			key = true;
+		if(str.equalsIgnoreCase("OFF"))
+			key = true;
+		if(str.equalsIgnoreCase("ON"))
+			key = true;
+		if(str.equalsIgnoreCase("OPEN"))
+			key = true;
+		if(str.equalsIgnoreCase("ORIGIN"))
+			key = true;
+		if(str.equalsIgnoreCase("PASSENGER"))
+			key = true;
+		if(str.equalsIgnoreCase("PATH"))
+			key = true;
+		if(str.equalsIgnoreCase("PAUSE"))
+			key = true;
+		if(str.equalsIgnoreCase("POLE"))
+			key = true;
+		if(str.equalsIgnoreCase("POLES"))
+			key = true;
+		if(str.equalsIgnoreCase("POSITION"))
+			key = true;
+		if(str.equalsIgnoreCase("POWER"))
+			key = true;
+		if(str.equalsIgnoreCase("PRIMARY"))
+			key = true;
+		if(str.equalsIgnoreCase("PROCEED"))
+			key = true;
+		if(str.equalsIgnoreCase("QUOTE"))
+			key = true;
+		if(str.equalsIgnoreCase("RANGE"))
+			key = true;
+		if(str.equalsIgnoreCase("RATE"))
+			key = true;
+		if(str.equalsIgnoreCase("REAL"))
+			key = true;
+		if(str.equalsIgnoreCase("REFERENCE"))
+			key = true;
+		if(str.equalsIgnoreCase("RESPOND"))
+			key = true;
+		if(str.equalsIgnoreCase("RESUME"))
+			key = true;
+		if(str.equalsIgnoreCase("ROUNDHOUSE"))
+			key = true;
+		if(str.equalsIgnoreCase("SCREEN"))
+			key = true;
+		if(str.equalsIgnoreCase("SECONDARY"))
+			key = true;
+		if(str.equalsIgnoreCase("SEGMENT"))
+			key = true;
+		if(str.equalsIgnoreCase("SELECT"))
+			key = true;
+		if(str.equalsIgnoreCase("SEMAPHORE"))
+			key = true;
+		if(str.equalsIgnoreCase("SEMICOLON"))
+			key = true;
+		if(str.equalsIgnoreCase("SENSOR"))
+			key = true;
+		if(str.equalsIgnoreCase("SENSORS"))
+			key = true;
+		if(str.equalsIgnoreCase("SET"))
+			key = true;
+		if(str.equalsIgnoreCase("SIGNAL"))
+			key = true;
+		if(str.equalsIgnoreCase("SINGLE_LINE_COMMENT"))
+			key = true;
+		if(str.equalsIgnoreCase("SLASH"))
+			key = true;
+		if(str.equalsIgnoreCase("SPEED"))
+			key = true;
+		if(str.equalsIgnoreCase("SPURS"))
+			key = true;
+		if(str.equalsIgnoreCase("STAR"))
+			key = true;
+		if(str.equalsIgnoreCase("START"))
+			key = true;
+		if(str.equalsIgnoreCase("STATION"))
+			key = true;
+		if(str.equalsIgnoreCase("STEAM"))
+			key = true;
+		if(str.equalsIgnoreCase("STOCK"))
+			key = true;
+		if(str.equalsIgnoreCase("STOP"))
+			key = true;
+		if(str.equalsIgnoreCase("STRAIGHT"))
+			key = true;
+		if(str.equalsIgnoreCase("STRING"))
+			key = true;
+		if(str.equalsIgnoreCase("SUBSTATION"))
+			key = true;
+		if(str.equalsIgnoreCase("SUBSTATIONS"))
+			key = true;
+		if(str.equalsIgnoreCase("SUPPLY"))
+			key = true;
+		if(str.equalsIgnoreCase("SWITCH"))
+			key = true;
+		if(str.equalsIgnoreCase("SWITCHER"))
+			key = true;
+		if(str.equalsIgnoreCase("SYNC"))
+			key = true;
+		if(str.equalsIgnoreCase("TANK"))
+			key = true;
+		if(str.equalsIgnoreCase("TENDER"))
+			key = true;
+		if(str.equalsIgnoreCase("THAN"))
+			key = true;
+		if(str.equalsIgnoreCase("TICK"))
+			key = true;
+		if(str.equalsIgnoreCase("TO"))
+			key = true;
+		if(str.equalsIgnoreCase("TOKENIMAGE"))
+			key = true;
+		if(str.equalsIgnoreCase("TOWARD"))
+			key = true;
+		if(str.equalsIgnoreCase("TRACK"))
+			key = true;
+		if(str.equalsIgnoreCase("TRACKS"))
+			key = true;
+		if(str.equalsIgnoreCase("TURNOUT"))
+			key = true;
+		if(str.equalsIgnoreCase("TURNTABLE"))
+			key = true;
+		if(str.equalsIgnoreCase("UNCOUPLE"))
+			key = true;
+		if(str.equalsIgnoreCase("UNDERSCORE"))
+			key = true;
+		if(str.equalsIgnoreCase("UNSYNC"))
+			key = true;
+		if(str.equalsIgnoreCase("UP"))
+			key = true;
+		if(str.equalsIgnoreCase("UPDATE"))
+			key = true;
+		if(str.equalsIgnoreCase("USE"))
+			key = true;
+		if(str.equalsIgnoreCase("VIEW"))
+			key = true;
+		if(str.equalsIgnoreCase("WATER"))
+			key = true;
+		if(str.equalsIgnoreCase("WHEN"))
+			key = true;
+		if(str.equalsIgnoreCase("WIDTH"))
+			key = true;
+		if(str.equalsIgnoreCase("WITH"))
+			key = true;
+		if(str.equalsIgnoreCase("WORLD"))
+			key = true;
+		if(str.equalsIgnoreCase("WYE"))
+			key = true;
+		if(str.equalsIgnoreCase("DIRECTION"))
+			key = true;
+		if(str.equalsIgnoreCase("ACTUATORS"))
+			key = true;
+		
+		return key;
+	}
 }
